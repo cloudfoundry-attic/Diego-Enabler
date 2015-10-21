8631ce0f40c499a239d8ee318684f06a90fd8e93
Kris Hicks <khicks@pivotal.io>
2015-10-20 17:02:41 -0700

    set binary output name on gox in build.sh
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

--- bin/build.sh
+++ bin/build.sh
@@ -8,8 +8,8 @@ fi
 
 rm -f diego-enabler*
 
-gox -os linux -os windows -arch 386
-gox -os darwin -os linux -os windows -arch amd64
+gox -os linux -os windows -arch 386 --output="diego-enabler_{{.OS}}_{{.Arch}}"
+gox -os darwin -os linux -os windows -arch amd64 --output="diego-enabler_{{.OS}}_{{.Arch}}"
 
 rm -rf out
 mkdir -p out
